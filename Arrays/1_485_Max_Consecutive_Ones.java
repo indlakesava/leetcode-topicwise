8d137c0fa03ad79bb88757e926d5148ccf9bfda0
indlakesava <50535274+indlakesava@users.noreply.github.com>
2023-10-23 23:07:05 -0400

    Additional solution for 1_485

--- Arrays/1_485_Max_Consecutive_Ones.java
+++ Arrays/1_485_Max_Consecutive_Ones.java
@@ -17,7 +17,9 @@ Constraints:
 nums[i] is either 0 or 1.
 */
 
-//Java Solution
+
+
+//Sliding Window Java Solution
 class Solution {
     public int findMaxConsecutiveOnes(int[] nums) {
         int i = 0;
@@ -41,6 +43,38 @@ class Solution {
  * We are using 2 pointer approach which could have also been done through current_max and globla_max approach
  * We use these 2 pointer to plot both the edges of the consecutive ones and calculate the length 
  * Once the length is found we check if this can be the maximum length by comparing with the macimum consecutive one till now
+ * 
+ * Time complexity - O(n)
+ * Space complexity - O(1) 
+ */
+
+
+
+//Current_max, Global_max Java Solution
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int result = 0;
+        int count = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+        	count++;
+        	result = Math.max(count, result);
+            }
+            else count = 0;
+        }
+        
+        return result;
+    }
+}
+
+/*
+ * Explanation:
+ * We are using current_max and globla_max approach
+ * We use result to store the global max and count to store the current_max
+ * Once we start looping through the array there can be one of the 2 cases we might encounter 1 or 0
+ * if we see 1 we keep iterating else if we come across 0 we then check the current_max and try to compare with global_max and update accordingly
+ * and set the current_max to 0 and continue with further iterations until the end of the array
  * 
  * Time complexity - O(n)
  * Space complexity - O(1) 
